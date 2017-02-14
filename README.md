9b1df507b836dda19b01a62a7d804f803ecc4abb
NielsHolt <niels@steenbuchholt.dk>
2017-02-14 12:59:58 +0100

    Version 2.0.0

--- README.md
+++ README.md
@@ -14,6 +14,21 @@ http://FCOO.github.io/leaflet-control-fa-button/demo/
 
 ## Usage
 
+	var myFaButton =  L.control.fontAwesomeButton( options );
+
+### Options
+| Id | Default | Description |
+| :--: |  :--: |  :--- |
+| `position` | `"topleft"` | Leaflet position |
+| `containerClassName` | `""` | Extra class-name(s) for the container |
+| `defaultIconClassName` | `"fa fa-lg"` | Standard FontAwesome classes |
+| `iconClassName` | `""` | The class defining the icon |
+| `data` | `"{}"` | data-attributes for the button |
+| `title` | `""` | The `title` for the button |
+| `href` | `undefined` | Link to page |
+| `onClick` | `null` | `function (evt, $button, options)` The function to be called |
+
+
 
 ## Copyright and License
 This plugin is licensed under the [MIT license](https://github.com/FCOO/leaflet-control-fa-button/LICENSE).
@@ -23,16 +38,5 @@ Copyright (c) 2015 [FCOO](https://github.com/FCOO)
 ## Contact information
 
 Jesper Larsen jla@fcoo.dk
-
-
-## Credits and acknowledgements
-
-
-## Known bugs
-
-## Troubleshooting
-
-## Changelog
-
-
+Niels Holt nho@fcoo.dk
 
--- dist/leaflet-control-fa-button.js
+++ dist/leaflet-control-fa-button.js
@@ -1,77 +1,76 @@
 /****************************************************************************
-	leaflet-control-fa-button.js, 
+    leaflet-control-fa-button.js, 
 
-	(c) 2015, FCOO
+    (c) 2015, FCOO
 
-	https://github.com/FCOO/leaflet-control-fa-button
-	https://github.com/FCOO
+    https://github.com/FCOO/leaflet-control-fa-button
+    https://github.com/FCOO
 
 ****************************************************************************/
-(function (/*$,*/ L, window, document, undefined) {
-	"use strict";
-
-	L.Control.FontAwesomeButton = L.Control.extend({
-    options: {
-			VERSION: "1.0.2",
-      position: 'topleft',
-      title: '',
-      href: undefined,
-      message: 'Press ' + (window.navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to save current map state in a bookmark.',
-      onclick: function (evt) {
-        evt.preventDefault(); // Prevent link from being processed
-        var bookmarkURL = window.location.href;
-        var bookmarkTitle = document.title;
-        var triggerDefault = false;
-        if (window.external && ('AddFavorite' in window.external)) {
-            // IE Favorite
-            window.external.AddFavorite(bookmarkURL, bookmarkTitle);
-        } else {
-            // WebKit - Safari/Chrome - Mozilla Firefox
-            alert(this.options.message);
-        }
-        return triggerDefault;
-      },
-      className: 'leaflet-control-bolt-button',
-      fontAwesomeIcon: "fa-bolt",
-    },
-    onAdd: function (map) {
-      this._map = map;
-      return this._initLayout();
-    },
-    _initLayout: function () {
-      var container = L.DomUtil.create('div',
-        'leaflet-bar leaflet-control-fa-button ' + this.options.className);
-      this._container = container;
-      this._button = this._createButton(container);
-
-      L.DomEvent.disableClickPropagation(container);
-      if (this.options.href === undefined) {
-        L.DomEvent.addListener(container, 'click', this.options.onclick, this);
-      }
-      return this._container;
-    },
-    _createButton: function () {
-      var link = L.DomUtil.create('a', 'fa ' + this.options.fontAwesomeIcon,
-        this._container);
-      if (this.options.href !== undefined) {
-        link.href = this.options.href;
-      }
-      link.title = this.options.title;
-
-      L.DomEvent
-        .on(link, 'mousedown dblclick', L.DomEvent.stopPropagation);
-        //.on(link, 'click', L.DomEvent.stop)
-      return link;
-    },
-  });
-
-  L.control.fontAwesomeButton = function (options) {
-    return new L.Control.FontAwesomeButton(options);
-  };
-
-  return L.Control.FontAwesomeButton;
-
-}(/*jQuery,*/ L, this, document));
+(function ($, L, window, document, undefined) {
+    "use strict";
 
 
+    function onClick( e ){
+        var $button = $(e.currentTarget),
+            options = $button.data('fontawesomebutton');
 
+        e.preventDefault(); // Prevent link from being processed
+
+        if (options && options.onClick)
+            return options.onClick( e, $button, options ); 
+        return false;
+    }
+
+    L.Control.FontAwesomeButton = L.Control.extend({
+        options: {
+            VERSION             : "2.0.0",
+            position            : 'topleft',
+            containerClassName  : '',
+            defaultIconClassName: 'fa fa-lg',
+            iconClassName       : '',
+            data                : {},
+            title               : '',
+            href                : undefined,
+            onClick             : null, //function (evt, $button, options) {...}
+        },
+        onAdd: function (map) {
+            this._map = map;
+
+            this._container = L.DomUtil.create('div', 'leaflet-bar leaflet-control-fa-button ' + this.options.containerClassName);
+
+            this._button = L.DomUtil.create('a', '', this._container);
+
+            this._button.title = this.options.title;
+            
+            var $button = $(this._button);
+            $button.data('fontawesomebutton', this.options );
+
+            //Adding 'data-'. Using $.attr() to make 'data-XX' avaiable for search
+            $.each( this.options.data, function( key, value ){
+                $button.attr('data-' + key, value );
+                $button.data( key, value );
+            });
+
+            //Add events
+            L.DomEvent.on(this._button, 'mousedown dblclick', L.DomEvent.stopPropagation);
+            L.DomEvent.disableClickPropagation( this._container );
+            if (this.options.href !== undefined)
+                this._button.href = this.options.href;
+            else 
+                $(this._button).on( 'click', onClick );
+
+            //Add the icon
+            L.DomUtil.create('i', this.options.defaultIconClassName + ' ' + this.options.iconClassName, this._button);
+           
+            return this._container;
+        },
+    });
+
+    L.control.fontAwesomeButton = function (options) {
+        return new L.Control.FontAwesomeButton(options);
+    };
+
+    return L.Control.FontAwesomeButton;
+
+}(jQuery, L, this, document));
